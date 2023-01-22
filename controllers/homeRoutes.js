a57fdce7a997a6e98f603fcbe2b0230dac34c908
John Jamieson <jamieson.john@gmail.com>
2023-01-22 16:44:22 -0500

    Added routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,80 @@
 const router = require('express').Router();
+const { Blog, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (request, response) => {
-    response.render('homepage');
+    try {
+        // Get all projects and JOIN with user data
+        const blogData = await Blog.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        // Serialize data so the template can read it
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        response.render('homepage', {
+            blogs,
+            logged_in: request.session.logged_in
+        });
+    } catch (error) {
+        response.status(500).json(error);
+    }
+});
+
+router.get('/blog/:id', async (request, response) => {
+    try {
+        const blogData = await Blog.findByPk(request.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const blog = blogData.get({ plain: true });
+
+        response.render('blog', {
+            ...blog,
+            logged_in: request.session.logged_in
+        });
+    } catch (error) {
+        response.status(500).json(error);
+    }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (request, response) => {
+    try {
+        // Find the logged in user based on the session ID
+        const userData = await User.findByPk(request.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Blog }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        response.render('dashboard', {
+            ...user,
+            logged_in: true
+        });
+    } catch (error) {
+        response.status(500).json(error);
+    }
 });
 
 router.get('/login', (request, response) => {
     // If the user is already logged in, redirect the request to another route
-/*    if (request.session.logged_in) {
-        response.redirect('/profile');
+    if (request.session.logged_in) {
+        response.redirect('/dashboard');
         return;
-    }*/
+    }
 
     response.render('login');
 });
--- server.js
+++ server.js
@@ -1,18 +1,31 @@
-const express = require('express')
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const app = express()
-const port = 3000
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const port = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create();
+const hbs = exphbs.create({ helpers });
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(routes);
 
-app.listen(port, () => {
-    console.log(`Server listening on: http://localhost:${port}`)
-})
+sequelize.sync({ force: false }).then(() => {
+    app.listen(port, () => {
+        console.log(`Server listening on: http://localhost:${port}`)
+    });
+});
